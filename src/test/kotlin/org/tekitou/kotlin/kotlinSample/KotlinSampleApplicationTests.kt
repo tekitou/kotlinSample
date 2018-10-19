54e530756913a730e9dbd6f1c01150f2da58025f
yusuke-katagiri <yusuke.katagiri@linecorp.com>
2018-10-19 14:08:41 +0900

    NO-ISSUE Initial

---
+++ src/test/kotlin/org/tekitou/kotlin/kotlinSample/KotlinSampleApplicationTests.kt
@@ -0,0 +1,16 @@
+package org.tekitou.kotlin.kotlinSample
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class KotlinSampleApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
